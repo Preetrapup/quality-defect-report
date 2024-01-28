8cb2315d7ec0b91ee80c85e93895d70d28a9771c
Darshan <darshan@illuminz.com>
2024-01-28 21:12:29 +0530

    ty

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: 'dist',
-  images: {
-    unoptimized: true
-  },
-  basePath: '/quality-defect-report',
-  assetPrefix: '/quality-defect-report',
-};
+const nextConfig = {}
+// {
+//   distDir: 'dist',
+//   images: {
+//     unoptimized: true
+//   },
+//   basePath: '/quality-defect-report',
+//   assetPrefix: '/quality-defect-report',
+// };
 
 export default nextConfig;
