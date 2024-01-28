7f9af6a5fcbb37a410c503407b0db7e34c967a52
Darshan <darshan@illuminz.com>
2024-01-28 20:45:33 +0530

    tt

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
   distDir: 'dist',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/quality-defect-report',
+  assetPrefix: '/quality-defect-report',
 };
 
 export default nextConfig;
